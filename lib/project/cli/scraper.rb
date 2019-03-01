c0f6ae71fdb8f27df4c84a0605bf32e8f19c7447
blackwell <blackwellsmithiv@gmail.com>
2019-03-01 15:44:16 +0000

    found first cheese in html with first cheese available

--- lib/project/cli/scraper.rb
+++ lib/project/cli/scraper.rb
@@ -1,11 +1,13 @@
 require "open-uri"
-
+require 'nokogiri'
+require 'pry'
 class Scraper
-  attr_accessor :name, :url 
+  
 
   def cheese_scraper
     doc = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/List_of_European_cheeses_with_protected_geographical_status#Netherlands"))
     dutch_cheese = doc.css("table")[9]
+    first_cheese = dutch_cheese.css("tr")[1]
     binding.pry
   end
 end
