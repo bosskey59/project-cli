d0d8303da861bfe59dfd04221f4ad07434f10835
blackwell <blackwellsmithiv@gmail.com>
2019-03-01 17:39:53 +0000

    added variables for attribute values of cheeses

--- lib/project/cli/scraper.rb
+++ lib/project/cli/scraper.rb
@@ -8,12 +8,12 @@ class Scraper
   def cheese_scraper
     doc = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/List_of_European_cheeses_with_protected_geographical_status#Netherlands"))
     dutch_cheese = doc.css("table")[9]
-    first_cheese = dutch_cheese.css("tr")[1]
-    the_big_cheese = first_cheese.xpath("//a")
-        the_big_cheese.each do |tag|
-      puts "#{a[:href]}\t#{tag.text}"
-      binding.pry
+    first_cheese = dutch_cheese.css("tr")[2]
+    the_big_cheese_url = "https://en.wikipedia.org"+(first_cheese.css("a").attribute("href").value)
+    the_big_cheese_name = first_cheese.css("a").attribute("title").value
+    the_big_cheese_location = first_cheese.css("a").text
+    binding.pry
     end
   end
-end
+
 Scraper.new.cheese_scraper
