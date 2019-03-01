9411dd3a5d63f6e8bdf0408e22b8b754be1d5d16
blackwell <blackwellsmithiv@gmail.com>
2019-03-01 16:51:20 +0000

    added xpath first_cheese

--- lib/project/cli/scraper.rb
+++ lib/project/cli/scraper.rb
@@ -2,13 +2,14 @@ require "open-uri"
 require 'nokogiri'
 require 'pry'
 class Scraper
+  attr_accessor :name, :url
   
 
   def cheese_scraper
     doc = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/List_of_European_cheeses_with_protected_geographical_status#Netherlands"))
     dutch_cheese = doc.css("table")[9]
     first_cheese = dutch_cheese.css("tr")[1]
-    the_big_cheese = first_cheese.css("tr td a")
+    the_big_cheese = first_cheese.xpath("//a")
     binding.pry
   end
 end
