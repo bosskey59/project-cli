be7ce10f11fa0aec706314441d56e604c028541e
blackwell <blackwellsmithiv@gmail.com>
2019-03-01 19:34:38 +0000

    trying anything

--- lib/project/cli/scraper.rb
+++ lib/project/cli/scraper.rb
@@ -4,15 +4,17 @@ require 'pry'
 class Scraper
   attr_accessor :name, :url, :place_of_origin
   
-  def self.cheese_array
-    cheeses = []
+  def cheese_array
+    @@cheeses = []
     
-    cheeses << self.cheese_scraper
-    binding.pry
+    @@cheeses << cheese_scraper
+    
   end
   
 
   def cheese_scraper
+    @@cheeses = []
+    
     doc = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/List_of_European_cheeses_with_protected_geographical_status#Netherlands"))
     dutch_cheese = doc.css("table")[9]
     i = 1
@@ -23,11 +25,12 @@ class Scraper
     cheese.url = "https://en.wikipedia.org"+(first_cheese.css("a").attribute("href").value)
     cheese.name = first_cheese.css("a").attribute("title").value
     cheese.place_of_origin = first_cheese.css("a").text
+    @@cheeses << cheese
     i += 1
     end
-    cheese
-   # binding.pry
-    end
+    @@cheeses
+    binding.pry
+     end
   end
 
 Scraper.new.cheese_array
