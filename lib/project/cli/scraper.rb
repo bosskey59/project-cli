944f1d0db5dc517c62576941498992cbfd972773
blackwell <blackwellsmithiv@gmail.com>
2019-03-01 15:12:40 +0000

    added dutch variable to cheese scraper method

--- lib/project/cli/scraper.rb
+++ lib/project/cli/scraper.rb
@@ -5,5 +5,8 @@ class Scraper
 
 def cheese_scraper
   doc = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/List_of_European_cheeses_with_protected_geographical_status#Netherlands"))
+  dutch_cheese = doc.css("table")[9]
+  binding.pry
   end
 end
+Scraper.cheese_scraper
