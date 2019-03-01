1d977a3981f15ef1644fb554e4cfcfcace52a6f3
blackwell <blackwellsmithiv@gmail.com>
2019-03-01 17:55:48 +0000

    added cheese array method and refactored chess scraper

--- lib/project/cli/scraper.rb
+++ lib/project/cli/scraper.rb
@@ -2,16 +2,26 @@ require "open-uri"
 require 'nokogiri'
 require 'pry'
 class Scraper
-  attr_accessor :name, :url
+  attr_accessor :name, :url, :place_of_origin
+  
+  def self.cheese_array
+    cheeses = []
+    
+    cheeses << self.cheese_scraper
+  end
   
 
   def cheese_scraper
     doc = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/List_of_European_cheeses_with_protected_geographical_status#Netherlands"))
     dutch_cheese = doc.css("table")[9]
     first_cheese = dutch_cheese.css("tr")[2]
-    the_big_cheese_url = "https://en.wikipedia.org"+(first_cheese.css("a").attribute("href").value)
-    the_big_cheese_name = first_cheese.css("a").attribute("title").value
-    the_big_cheese_location = first_cheese.css("a").text
+    
+    cheese = self.new
+    cheese.url = "https://en.wikipedia.org"+(first_cheese.css("a").attribute("href").value)
+    cheese.name = first_cheese.css("a").attribute("title").value
+    cheese.place_of_origin = first_cheese.css("a").text
+    
+    cheese
     binding.pry
     end
   end
