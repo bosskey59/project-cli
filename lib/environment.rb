1e0af50efc90d33b02ea4144280d64a422c74f22
blackwell <blackwellsmithiv@gmail.com>
2019-03-04 12:52:33 +0000

    fixed environment

--- lib/environment.rb
+++ lib/environment.rb
@@ -2,6 +2,6 @@ require "open-uri"
 require "nokogiri"
 require "pry"
 
-require_relative "./project/cli/controller"
+require_relative "./project/cli/controller.rb"
 require_relative "./project/cli/scraper.rb"
 require_relative "./project/cli/version.rb"
---
+++ lib/project/cli/controller.rb
@@ -0,0 +1,4 @@
+require_relative ".lib/project/cli/scraper.rb"
+
+class Controller
+end
