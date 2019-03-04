de92c4fc544618f83523e1ac8f4b4c3670012070
blackwell <blackwellsmithiv@gmail.com>
2019-03-04 13:46:29 +0000

    added call method to con troller

--- lib/project/cli/controller.rb
+++ lib/project/cli/controller.rb
@@ -1,4 +1,9 @@
 require_relative ".lib/project/cli/scraper.rb"
 
 class Controller
+  
+  def call
+    puts "So I hear you like cheese."
+    puts "If you would like to see a list of protected Dutch cheeses type 'list cheeses'. "
+  
 end
