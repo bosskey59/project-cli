2026d72591e6997281f52a508b8bd486bf151d36
blackwell <blackwellsmithiv@gmail.com>
2019-03-04 14:02:27 +0000

    added list_cheese to controller class

--- lib/project/cli/controller.rb
+++ lib/project/cli/controller.rb
@@ -5,5 +5,20 @@ class Controller
   def call
     puts "So I hear you like cheese."
     puts "If you would like to see a list of protected Dutch cheeses type 'list cheeses'. "
+    
+    
+    
+    input = gets.strip.downcase
+    
+    case 
+    when "list cheeses"
+      list_cheese
+      
+  end
   
+  def list_cheeses
+     Scraper.all.each.with_index(1) do |chz, i|
+     puts "#{i}. #{chz.name}"
+  end
+  end
 end
